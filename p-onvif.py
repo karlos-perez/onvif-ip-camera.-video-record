41302d93268405eaa7e48b7cb481b364f1aa34c4
karlos <kralole@gmail.com>
2016-08-28 09:52:15 +0300

    first commit

---
+++ p-onvif.py
@@ -0,0 +1,252 @@
+#!/usr/bin/env python3
+
+import datetime
+from hashlib import sha1
+import uuid
+import logging
+log = logging.getLogger(__name__)
+D = log.debug
+
+from random import SystemRandom
+import string
+import base64
+import time
+
+import requests
+from bs4 import BeautifulSoup
+
+
+class OnvifCam:
+    def __init__(self):
+        pass
+
+    def setup(self, ipaddr, ipport, user, psw):
+        self.ip = ipaddr
+        self.port = ipport
+        self.username = user
+        self.password = psw
+        # self.url = 'http://{ip}:{port}/onvif/Events'.format(ip=self.ip, port=self.port)
+        prof = self.getProfiles()[0]
+        self.profilename = list(prof.keys())[0]
+        self.profiletoken = prof[self.profilename]
+
+    # TODO: ?????
+    def onvifAuthHeader(self):
+        created = datetime.datetime.now().isoformat().split(".")[0]
+        n64 = ''.join(SystemRandom().choice(string.ascii_letters + string.digits+string.punctuation) for _ in range(22))
+        nc = base64.b64encode(n64.encode())
+        conc = n64 + created + self.password
+        pdigest= base64.b64encode(sha1(conc.encode()).digest())
+        username = '<Username>{}</Username>'.format(self.username)
+        password= '<Password Type="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordDigest">{}</Password>'.format(pdigest.decode())
+        nonce = '<Nonce EncodingType="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-soap-message-security-1.0#Base64Binary">{}</Nonce>'.format(nc.decode())
+        created = '<Created xmlns="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd">{}</Created>'.format(created)
+        usertoken= '<UsernameToken>{}{}{}{}</UsernameToken>'.format(username, password, nonce, created)
+        header = '<s:Header><Security s:mustUnderstand="1" xmlns="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd">{}</Security></s:Header>'.format(usertoken)
+        return header
+
+    def _createSOAPmsg(self, msg, header=''):
+        envelope = '<?xml version="1.0" encoding="UTF-8"?><s:Envelope xmlns:s="http://www.w3.org/2003/05/soap-envelope" xmlns:a="http://www.w3.org/2005/08/addressing">{}</s:Envelope>'
+        body = '<s:Body xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">{}</s:Body>'.format(msg)
+        if header:
+            fullmsg = '{}{}'.format(header, body)
+            soapmsg = envelope.format(fullmsg)
+        else:
+            soapmsg = envelope.format(body)
+        return soapmsg
+
+    def _sendRequest(self, url, msg):
+        headers = {'Content-Type':'application/soap+xml; charset=utf-8'}
+        response = requests.post(url, msg, headers=headers)
+         # TODO: переделать формат return {'error': False, 'response': BeautifulSoup(response.content, 'lxml')}
+        if response.status_code == 200:
+            return  BeautifulSoup(response.content, 'lxml')
+        elif response.status_code == 400:
+            resp = BeautifulSoup(response.content, 'lxml')
+            raise Exception(resp.find('soap-env:text').text)
+        else:
+            print(response.content.decode())
+            raise Exception("Status code: ", response.status_code)
+
+    # TODO: remove s
+    def _getAllNodeRecursively(self, rt, s, res):
+        try:
+            name = (rt.name).split(':')[1]
+        except:
+            name = rt.name
+        # if rt.attrs:
+        #     print('ATRIB: ', name,  rt.attrs)
+        if len(list(rt.findChildren())) == 0:
+            # print(' '*s, name, '---', rt.text)
+            res[name] = rt.text
+        else:
+            # print(' '*s, name)
+            res[name] = {}
+            s += 4
+            for i in rt.contents:
+                self._getAllNodeRecursively(i, s, res[name])
+
+    def getDeviceInformation(self):
+        url = 'http://{ip}:{port}/onvif/device_service'.format(ip=self.ip, port=self.port)
+        msg='<GetDeviceInformation xmlns="http://www.onvif.org/ver10/device/wsdl"/>'
+        resp = self._sendRequest(url, self._createSOAPmsg(msg))
+        result = {}
+        self._getAllNodeRecursively(resp.find('tds:getdeviceinformationresponse'), 0, result)
+        return result['getdeviceinformationresponse']
+
+    def getCapabilities(self):
+        url = 'http://{ip}:{port}/onvif/device_service'.format(ip=self.ip, port=self.port)
+        msg = '<GetCapabilities xmlns="http://www.onvif.org/ver10/device/wsdl"><Category>All</Category></GetCapabilities>'
+        resp = self._sendRequest(url, self._createSOAPmsg(msg))
+        result = {}
+        self._getAllNodeRecursively(resp.find('tds:capabilities'), 0, result)
+        return result['capabilities']
+
+    def getSystemDateAndTime(self):
+        url = 'http://{ip}:{port}/onvif/device_service'.format(ip=self.ip, port=self.port)
+        msg = '<GetSystemDateAndTime xmlns="http://www.onvif.org/ver10/device/wsdl"/>'
+        resp = self._sendRequest(url, self._createSOAPmsg(msg))
+        sdt = {}
+        self._getAllNodeRecursively(resp.find('tds:systemdateandtime'), 0, sdt)
+        result = {}
+        result['timezone'] = sdt['systemdateandtime']['timezone']['tz']
+        date = sdt['systemdateandtime']['localdatetime']['date']
+        time = sdt['systemdateandtime']['localdatetime']['time']
+        # TODO: 2016818-21712
+        dt = datetime.datetime(int(date['year']),
+                               int(date['month']),
+                               int(date['day']),
+                               int(time['hour']),
+                               int(time['minute']),
+                               int(time['second']))
+        result['date'] = '{:%Y%m%d-%H%M%S}'.format(dt)
+        return result
+
+    def getServiceCapabilities(self):
+        url = 'http://{ip}:{port}/onvif/device_service'.format(ip=self.ip, port=self.port)
+        msg = '<GetServiceCapabilities xmlns="http://www.onvif.org/ver10/device/wsdl"></GetServiceCapabilities>'
+        resp = self._sendRequest(url, self._createSOAPmsg(msg))
+        result = {}
+        for i in resp.find('tds:capabilities').contents:
+            result[(i.name).split(':')[1]] = i.attrs
+        return result
+
+    def getVideoSources(self):
+        url = 'http://{ip}:{port}/onvif/Media'.format(ip=self.ip, port=self.port)
+        msg= '<GetVideoSources xmlns="http://www.onvif.org/ver10/media/wsdl"/>'
+        resp = self._sendRequest(url, self._createSOAPmsg(msg))
+        result = {}
+        self._getAllNodeRecursively(resp.find('trt:videosources'), 0, result)
+        return result['videosources']
+
+    def getStreamUri(self):
+        url = 'http://{ip}:{port}/onvif/Media'.format(ip=self.ip, port=self.port)
+        stream = '<Stream xmlns="http://www.onvif.org/ver10/schema">RTP-Unicast</Stream>'
+        protocol = '<Protocol>RTSP</Protocol>'
+        transport = '<Transport xmlns="http://www.onvif.org/ver10/schema">{}</Transport>'.format(protocol)
+        streamsetup = '<StreamSetup>{}{}</StreamSetup>'.format(stream, transport)
+        profiletoken = '<ProfileToken>{}</ProfileToken>'.format(self.profiletoken)
+        msg = '<GetStreamUri xmlns="http://www.onvif.org/ver10/media/wsdl">{}{}</GetStreamUri>'.format(streamsetup, profiletoken)
+        resp = self._sendRequest(url, self._createSOAPmsg(msg))
+        # print(resp.prettify())
+        return resp.find('tt:uri').text
+
+    def getProfiles(self):
+        url = 'http://{ip}:{port}/onvif/Media'.format(ip=self.ip, port=self.port)
+        msg='<GetProfiles xmlns="http://www.onvif.org/ver10/media/wsdl"/>'
+        resp = self._sendRequest(url, self._createSOAPmsg(msg))
+        result = []
+        for p in resp.find_all('trt:profiles'):
+            prof = p.findChild('tt:name')
+            pr = {}
+            pr[prof.text] = p.attrs['token']
+            result.append(pr)
+        return result
+        # print(resp.prettify())
+        # print(resp.find_all())
+        # print(resp.find_all('tt:name'))
+
+        # result = {}
+        # self.recurs(resp.find('trt:getprofilesresponse'), 0, result)
+        # return result
+        # a = resp.find('trt:getprofilesresponse')
+        # for i in a.contents:
+        #     print(i.name, i.attrs)
+        #     for j in i.contents:
+        #         if j.attrs:
+        #             print(j.name, j.attrs)
+        #         else:
+        #             print(j.name, '--------------')
+
+    def _createHeadPullMessages(self, urlact, urlto):
+        action =  '<a:Action s:mustUnderstand="1">{}</a:Action>'.format(urlact)
+        massageID = '<a:MessageID>urn:uuid:{}</a:MessageID>'.format(uuid.uuid4())
+        replyTo = '<a:ReplyTo><a:Address>"http://www.w3.org/2005/08/addressing/anonymous"</a:Address></a:ReplyTo>'
+        to = '<a:To s:mustUnderstand="1">{}</a:To>'.format(urlto)
+        header = '<s:Header>{}{}{}{}</s:Header>'.format(action, massageID, replyTo, to)
+        return header
+
+    def _convertStrToBool(self, string):
+        if string.lower() == 'false':
+            return False
+        else:
+            return True
+
+    def _sendPullMessages(self, url):
+        self.timeout = 'PT1M'
+        self.msglimit = 1024
+        tmout = '<Timeout>{}</Timeout>'.format(self.timeout)
+        messagelimit = '<MessageLimit>{}</MessageLimit>'.format(self.msglimit)
+        msg = '<PullMessages xmlns="http://www.onvif.org/ver10/events/wsdl">{}{}</PullMessages>'.format(tmout, messagelimit)
+        urlAction = 'http://www.onvif.org/ver10/events/wsdl/PullPointSubscription/PullMessagesRequest'
+        head = self._createHeadPullMessages(urlAction, url)
+        a = self._createSOAPmsg(msg, head)
+        resp = self._sendRequest(url, a)
+        return resp
+
+    def createPullPointSubscription(self):
+        url = 'http://{}:{}/onvif/Events'.format(self.ip, self.port)
+        urlAction = 'http://www.onvif.org/ver10/events/wsdl/EventPortType/CreatePullPointSubscriptionRequest'
+        bmsg = '<CreatePullPointSubscription xmlns="http://www.onvif.org/ver10/events/wsdl"><InitialTerminationTime>PT600S</InitialTerminationTime></CreatePullPointSubscription>'
+        h = self._createHeadPullMessages(urlAction, url)
+        msg = self._createSOAPmsg(bmsg, h)
+        resp = self._sendRequest(url, msg)
+        addr = resp.find('tev:subscriptionreference').findChild('wsa5:address').text
+
+
+        # TODO: !!!!!!
+        for i in range(100):
+            a = self._sendPullMessages(addr)
+            # print(a.prettify())
+            # print('-----------')
+            tt = a.find('tev:currenttime').text
+            aa = a.find('tt:data')
+            aaa = aa.findChild().attrs
+            motion = aaa['value']
+            print('{}.  {} - {}'.format(i, tt, motion))
+            time.sleep(1)
+        return '!!!!!!!!!!!!!!'
+
+
+if __name__ == "__main__":
+    ipaddr = "172.16.0.7"
+    port = "8899"
+    cam=OnvifCam()
+    cam.setup(ipaddr, port, 'admin', '')
+    print(cam.getDeviceInformation())
+    print('==='*30)
+    print(cam.getCapabilities())
+    print('==='*30)
+    print(cam.getSystemDateAndTime())
+    print('==='*30)
+    print(cam.getServiceCapabilities())
+    print('==='*30)
+    print(cam.getProfiles())
+    print('==='*30)
+    print(cam.getVideoSources())
+    print('==='*30)
+    print(cam.getStreamUri())
+    print('==='*30)
+    print(cam.createPullPointSubscription())
+
+
---
+++ p-rtsp.py
@@ -0,0 +1,302 @@
+#!/usr/bin/env python3
+
+import bitstring
+import logging
+import re
+import socket
+import time
+
+from multiprocessing import Process, Event
+
+
+LOG_FILE = 'rtsp_record.log'
+URL_CAM = "rtsp://172.16.0.7:554/user=admin&password=&channel=1&stream=0.sdp"
+IP_CAM_ADDR = '172.16.0.7'
+IP_CAM_PORT = 554
+CLIENT_PORTS = [60784, 60785]
+
+
+class ClientRTSP(Process):
+    '''
+        Recording ip video camera with defined duration.
+        Video file format: 20160101-121134-0.h264
+    '''
+    m_describe = ("DESCRIBE {url} RTSP/1.0\r\n"
+                  "CSeq: 2\r\n"
+                  "User-Agent: python\r\n"
+                  "Accept: application/sdp\r\n\r\n").format(url=URL_CAM)
+    m_setup = ("SETUP {url} RTSP/1.0\r\n"
+              "CSeq: 3\r\n"
+              "User-Agent: python\r\n"
+              "Transport: RTP/AVP;unicast;client_port={port1}-{port2}\r\n"
+              "\r\n").format(url=URL_CAM, port1=CLIENT_PORTS[0], port2=CLIENT_PORTS[1])
+    m_play = ("PLAY {url} RTSP/1.0\r\n"
+              "CSeq: 5\r\n"
+              "User-Agent: python\r\n"
+              "Session: {id}\r\n"
+              "Range: npt=0.000-\r\n\r\n")
+    m_close = ("TEARDOWN {url} RTSP/1.0\r\nCSeq: 8\r\nSession: {id}\r\n\r\n")
+    format_file = '{}{}.h264'
+
+
+    def __init__(self):
+        self.rtsp_url = URL_CAM
+        self.msg_describe = self.m_describe.encode()
+        self.msg_setup = self.m_setup.encode()
+
+    def _id_session(self, response):
+        """
+            Search session id from rtsp strings
+        """
+        resp = response.split('\r\n')
+        for r in resp:
+            ss = r.split()
+            if ss[0].strip() == "Session:":
+                return int(ss[1].split(";")[0].strip())
+
+    def _RTP_handler(self, st):
+        """
+            This routine takes a UDP packet, i.e. a string of bytes and ..
+            (a) strips off the RTP header
+            (b) adds NAL "stamps" to the packets, so that they are recognized as NAL's
+            (c) Concantenates frames
+            (d) Returns a packet that can be written to disk as such and that is recognized
+                by stock media players as h264 stream
+        """
+        startbytes = b"\x00\x00\x00\x01"  # this is the sequence of four bytes that identifies a NAL packet
+        result = {}
+        bt = bitstring.BitArray(bytes=st)  # turn the whole string-of-bytes packet into a string of bits
+        lc = 12  # bytecounter
+        bc = 12 * 8  # bitcounter
+        # ******* Parse RTP header ********
+        version = bt[0:2].uint  # version
+        p = bt[2]  # P
+        x = bt[3]  # X
+        cc = bt[4:8].uint  # CC
+        m = bt[8]  # M
+        pt = bt[9:16].uint  # PT
+        sn = bt[16:32].uint  # sequence number
+        timestamp = bt[32:64].uint  # timestamp
+        ssrc = bt[64:96].uint  # ssrc identifier
+        logging.debug("version:{0}, p: {1}, x: {2}, cc: {3}, m: {4}, pt: {5}".format(version, p, x, cc, m, pt))
+        logging.debug("sequence number: {0}, timestamp: {1}".format(sn, timestamp))
+        logging.debug("sync. source identifier: {}".format(ssrc))
+        if cc:
+            cids = []
+            for i in range(cc):
+                cids.append(bt[bc:bc+32].uint)
+                bc += 32
+                lc += 4
+            logging.debug("csrc identifiers: {}".format(cids))
+        if x:
+            # TODO: check this section
+            logging.info('X - is True')
+            hid = bt[bc:bc+16].uint
+            bc += 16
+            lc += 2
+            hlen = bt[bc:bc+16].uint
+            bc += 16
+            lc += 2
+            logging.debug("ext. header id: {0}, ext. header len: {1}".format(hid, hlen))
+            bc += 32 * hlen
+            lc += 4 * hlen
+        # ********** NAL packet ************
+        # ********** Parse "First byte" a NAL packet ************
+        fb = bt[bc]  # "F"
+        nri = bt[bc+1:bc+3].uint  # "NRI"
+        nlu0 = bt[bc:bc+3]  # "3 NAL UNIT BITS" (i.e. [F | NRI])
+        typ = bt[bc+3:bc+8].uint  # "Type"
+        logging.debug("F: {0}, NRI: {1}, Type: {2}".format(fb, nri, typ))
+        if 0 <= typ <= 12:
+            if typ == 7:
+                logging.debug(">>>>> SPS packet")
+                result['typ'] = 'SPS'
+            elif typ == 8:
+                logging.debug(">>>>> PPS packet")
+                result['typ'] = 'PPS'
+            else:
+                logging.debug("Unknow TYPE packet: {}".format(typ))
+                result['typ'] = 'UNKW'
+            result['data'] = startbytes + st[lc:]
+            return result
+        # ********** Parse "Second byte" a NAL packet ************
+        if typ == 28:  # Handles only "Type" = 28, i.e. "FU-A"
+            bc += 8
+            lc += 1
+            start = bt[bc]  # start bit
+            end = bt[bc+1]  # end bit
+            nlu1 = bt[bc+3:bc+8]  # 5 nal unit bits
+            logging.debug("Start bit: {}".format(start))
+            logging.debug("End bit: {}".format(end))
+            logging.debug("Reserved bit: {}".format(bt[bc+2]))
+            result['start'] = start
+            result['end'] = end
+            head = b''
+            if start:
+                logging.debug(">>> first fragment found")
+                nlu = nlu0 + nlu1  # Create "[3 NAL UNIT BITS | 5 NAL UNIT BITS]"
+                head = startbytes + nlu.bytes  # add the NAL starting sequence
+                lc += 1
+            elif start == False and end == False:  # intermediate fragment in a sequence, just dump "VIDEO FRAGMENT DATA"
+                lc += 1  # Skip the "Second byte"
+            elif end:  # last fragment in a sequence, just dump "VIDEO FRAGMENT DATA"
+                logging.debug(">>> last fragment found")
+                lc += 1  # Skip the "Second byte"
+            result['data'] = head + st[lc:]
+            result['typ'] = 'FU'
+            return result
+        else:
+            logging.error("Unknown frame type ({}) for this fragment".format(typ))
+            raise(Exception, "unknown frame type for this piece of s***")
+
+    def _log_record(self, response):
+        """
+            Pretty-printing rtsp strings
+        """
+        resp = response.split('\r\n')
+        for r in resp:
+            logging.info(r)
+
+    def _get_ports(self, searchst, st):
+        """
+            Searching port numbers from rtsp strings using regular expressions
+        """
+        pat = re.compile(searchst+"=\d*-\d*")
+        pat2 = re.compile('\d+')
+        mstring = pat.findall(st)[0]
+        nums = pat2.findall(mstring)
+        numas = []
+        for num in nums:
+            numas.append(int(num))
+        return numas
+
+    def _filename(self, idr=''):
+        if idr:
+            id_rec = '-{}'.format(idr)
+        else:
+            id_rec = ''
+        return self.format_file.format(time.strftime("%Y%m%d-%H%M%S"), id_rec)
+
+    def _r(self, b, filename):
+        with open(filename, 'wb') as h264:
+            for i in b:
+                h264.write(i)
+
+    def _get_frame(self):
+        SPS_count = 0
+        chunk = []
+        while True:
+            resp = self.udp_socket.recv(4096)
+            st = self._RTP_handler(resp)
+            if st['typ'] == 'SPS':
+                SPS_count += 1
+                if SPS_count == 2:
+                    SPS_count -= 1
+                    yield chunk
+                    chunk.clear()
+            chunk.append(st['data'])
+
+    # TODO: how????
+    # def _ring_buffer(self, queue, size):
+        # buffer = deque()
+        # start_time = time.time()
+        # for i in self._get_frame():
+        #     if time.time() - start_time < size:
+        #         buffer.append(i)
+        #     else:
+        #         buffer.popleft()
+        #         buffer.append(i)
+        #     # print('len: ', len(i))
+        #     # print(len(buffer))
+        #     if ring_e.is_set():
+        #         g = list(buffer)
+        #         if queue.full():
+        #             queue.get()
+        #         queue.put(g)
+
+    def _record(self, filename, durations=None):
+        chunks = self._get_frame()
+        with open(filename, 'wb') as h264:
+            begin_rec = time.time()
+            for chunk in chunks:
+                for frame in chunk:
+                    h264.write(frame)
+                if stop_record.is_set():
+                    break
+                if durations:
+                    if time.time() - begin_rec > durations:
+                        break
+
+    def _make_send_msg(self, idr, msg):
+        result = msg.format(url=self.rtsp_url, id=idr)
+        return result.encode()
+
+    def _make_UDP_socket(self, ports):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        for p in ports:
+            try:
+                sock.bind(("", p))
+                break
+            except:
+                continue
+        sock.settimeout(5)
+        return sock
+
+    def _make_control_socket(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((IP_CAM_ADDR, IP_CAM_PORT))
+        return sock
+
+    def _start(self):
+        self.ctrl_socket = self._make_control_socket()
+        self.ctrl_socket.send(self.msg_describe)
+        self._log_record(self.ctrl_socket.recv(4096).decode())
+        self.ctrl_socket.send(self.msg_setup)
+        resp = self.ctrl_socket.recv(4096).decode()
+        self._log_record(resp)
+        id_session = self._id_session(resp)  # Get id session
+        self.msg_play = self._make_send_msg(id_session, self.m_play) # Make PLAY message
+        self.msg_close = self._make_send_msg(id_session, self.m_close)  # Make CLOSE message
+        clientports = self._get_ports("client_port", resp)
+        self.udp_socket = self._make_UDP_socket(clientports)
+        self.ctrl_socket.send(self.msg_play)
+        self._log_record(self.ctrl_socket.recv(4096).decode())
+
+    def _finish(self):
+        self.ctrl_socket.send(self.msg_close)
+        self._log_record(self.ctrl_socket.recv(4096).decode())
+        self.ctrl_socket.close()
+        self.udp_socket.close()
+        stop_record.clear()
+
+    def run_record(self, durations=None, id_record=None):
+        self._start()
+        if id_record:
+            filename =  self._filename(id_record)
+        else:
+            filename = self._filename()
+        self._record(filename, durations)
+        self._finish()
+
+
+
+
+
+if __name__ == "__main__":
+    log_file = LOG_FILE
+    log_level = logging.WARNING
+    log_format = '[%(asctime)s] %(levelname)-8s: %(message)s'
+    logging.basicConfig(level=log_level, format=log_format, filename=log_file)
+    dur = 10  # Durations record video in second
+    c = ClientRTSP()
+    # c.run_record(dur)
+    stop_record = Event()
+    proc = Process(target=c.run_record, args=(None, 1))
+    proc.daemon = True
+    proc.start()
+
+    print('start proccess')
+    time.sleep(10)
+    stop_record.set()
+    print('stop rec')
+    proc.join()
