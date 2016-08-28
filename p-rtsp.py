a258915a83143dd58edfb501290bf69c1053fa9a
karlos <kralole@gmail.com>
2016-08-28 12:13:49 +0300

    Add record video with pre-recording

--- p-rtsp.py
+++ p-rtsp.py
@@ -6,6 +6,7 @@ import re
 import socket
 import time
 
+from collections import deque
 from multiprocessing import Process, Event
 
 
@@ -196,25 +197,7 @@ class ClientRTSP(Process):
                     chunk.clear()
             chunk.append(st['data'])
 
-    # TODO: how????
-    # def _ring_buffer(self, queue, size):
-        # buffer = deque()
-        # start_time = time.time()
-        # for i in self._get_frame():
-        #     if time.time() - start_time < size:
-        #         buffer.append(i)
-        #     else:
-        #         buffer.popleft()
-        #         buffer.append(i)
-        #     # print('len: ', len(i))
-        #     # print(len(buffer))
-        #     if ring_e.is_set():
-        #         g = list(buffer)
-        #         if queue.full():
-        #             queue.get()
-        #         queue.put(g)
-
-    def _record(self, filename, durations=None):
+    def _record_online(self, filename, durations=None):
         chunks = self._get_frame()
         with open(filename, 'wb') as h264:
             begin_rec = time.time()
@@ -226,6 +209,41 @@ class ClientRTSP(Process):
                 if durations:
                     if time.time() - begin_rec > durations:
                         break
+
+    def _record_with_pre_buffer(self, size_buffer):
+        buffer = deque()
+        chunks = self._get_frame()
+        record = False
+        id_record = 0
+        start_buffer = time.time()
+        for chunk in chunks:
+            if start_record.is_set() and not record:
+                fn = self._filename(str(id_record))
+                h264 = open(fn, 'wb')
+                record = True
+            if time.time() - start_buffer < int(size_buffer):
+                buffer.append(chunk)
+                if record:
+                    for f in buffer.index(0):
+                        h264.write(f)
+            else:
+                if record:
+                    for f in buffer.popleft():
+                        h264.write(f)
+                else:
+                    buffer.popleft()
+                buffer.append(chunk)
+            if not start_record.is_set() and record:
+                record =False
+                id_record += 1
+                h264.close()
+                if stop_record.is_set():
+                    break
+            if stop_record.is_set():
+                if not start_record.is_set() and not record:
+                    break
+                else:
+                    start_record.clear()
 
     def _make_send_msg(self, idr, msg):
         result = msg.format(url=self.rtsp_url, id=idr)
@@ -255,7 +273,7 @@ class ClientRTSP(Process):
         resp = self.ctrl_socket.recv(4096).decode()
         self._log_record(resp)
         id_session = self._id_session(resp)  # Get id session
-        self.msg_play = self._make_send_msg(id_session, self.m_play) # Make PLAY message
+        self.msg_play = self._make_send_msg(id_session, self.m_play)  # Make PLAY message
         self.msg_close = self._make_send_msg(id_session, self.m_close)  # Make CLOSE message
         clientports = self._get_ports("client_port", resp)
         self.udp_socket = self._make_UDP_socket(clientports)
@@ -269,17 +287,19 @@ class ClientRTSP(Process):
         self.udp_socket.close()
         stop_record.clear()
 
-    def run_record(self, durations=None, id_record=None):
+    def run_record_online(self, durations=None, id_record=None):
         self._start()
         if id_record:
-            filename =  self._filename(id_record)
+            filename = self._filename(id_record)
         else:
             filename = self._filename()
-        self._record(filename, durations)
+        self._record_online(filename, durations)
         self._finish()
 
-
-
+    def run_record_with_prebuffer(self, size_buffer=5):
+        self._start()
+        self._record_with_pre_buffer(size_buffer)
+        self._finish()
 
 
 if __name__ == "__main__":
@@ -291,12 +311,28 @@ if __name__ == "__main__":
     c = ClientRTSP()
     # c.run_record(dur)
     stop_record = Event()
-    proc = Process(target=c.run_record, args=(None, 1))
+    start_record = Event()
+
+    proc = Process(target=c.run_record_with_prebuffer, args=(10,))
     proc.daemon = True
     proc.start()
 
-    print('start proccess')
-    time.sleep(10)
-    stop_record.set()
+    print('start proccess:')
+    time.sleep(15)
+    start_record.set()
+    print(time.strftime("%Y%m%d-%H%M%S"))
+    time.sleep(20)
+    start_record.clear()
     print('stop rec')
+    print(time.strftime("%Y%m%d-%H%M%S"))
+    time.sleep(15)
+    print('start proccess:')
+    start_record.set()
+    print(time.strftime("%Y%m%d-%H%M%S"))
+    time.sleep(20)
+    start_record.clear()
+    print('stop rec')
+    print(time.strftime("%Y%m%d-%H%M%S"))
+    stop_record.set()
     proc.join()
+    print('end')
